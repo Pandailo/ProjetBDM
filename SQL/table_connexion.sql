de28179b568c18a964ce44b31a41ddf40f977ab8
YannV <veron.yann@gmail.com>
2017-04-30 08:11:55 +0200

    modif table co

--- SQL/table_connexion.sql
+++ SQL/table_connexion.sql
@@ -1,2 +1,8 @@
-CREATE TABLE PBDM_table_connexion (uname VARCHAR2(25),pw VARCHAR2(25),droits ENUM('admin','user'));
+CREATE TABLE PBDM_table_connexion (
+	uname VARCHAR2(25),
+	pw VARCHAR2(25),
+	droits VARCHAR2(10) CHECK( 
+		droits IN ('admin','user') 
+							) 
+								);
 /
