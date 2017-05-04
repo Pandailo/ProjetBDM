9edcba3080285da5174ec02397e44ffd2ef3a461
Yann VERON <yv965015@MI114-01.iem>
2017-05-04 16:59:10 +0200

    Abandon reset BD

--- ProjetBDM/src/projetbdm/frame_menu.java
+++ ProjetBDM/src/projetbdm/frame_menu.java
@@ -5,6 +5,12 @@
  */
 package projetbdm;
 
+import java.io.IOException;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Yann
